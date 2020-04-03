b6a8f07f74685f1f70797c6fe0822e3f8b6dfe62
Antkeo1 <sleav143@gmail.com>
2020-04-03 13:18:33 -0400

    creating the forms

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -7,11 +7,63 @@ import AddCategory from './AddCategory'
 
 const AddProduct = () => {
     const {user, token} = isAuthenticated()
+    // defining our state into values with useState
+    const [values, setValues] = useState({
+        name: '',
+        description: '',
+        price: '',
+        categories: [],
+        category: '',
+        shipping: '',
+        quanity: '',
+        photo: '',
+        loading: false,
+        error: '',
+        createdProduct: '',
+        redirectToProfile: false,
+        formData: ''
+    })
+
+    // destructuring our state from values
+    const {
+        name,
+        description,
+        price,
+        categories,
+        category,
+        shipping,
+        quanity,
+        photo,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData,
+    } = values
+
+    const newProductForm = () => (
+        <form className='mb-5'>
+            <h4>Post Photo</h4>
+            <div className='form-group'>
+                <label className='btn btn-secondary'>
+                    <input type='file' name='photo' accept='image/*' />
+                </label>
+            </div>
+
+            <div className='form-group'>
+                <label>
+                    Name
+                </label>
+                <input type='text' className='form-control' />
+            </div>
+        </form>
+    )
+
     return (
         <Layout title='Add a new Product' description={`G'day ${user.name}, ready to add a new product?`} >
            <div className='row'>
                <div className='col-md-8 offset-md-2'>
-                   ...
+                   {newProductForm()}
                </div>
            </div>
 
