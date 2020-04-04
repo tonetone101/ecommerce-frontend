b69168c0b2530caf60ce49ccc6e43897bd8712d9
Antkeo1 <sleav143@gmail.com>
2020-04-04 15:09:33 -0400

    products now render on the home page

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import Layout from './Layout'
 import {getProducts} from './apiCore'
+import Card from './Card'
 
 const Home = () => {
     const [productsBySell, setProductsBySell] = useState([])
@@ -35,10 +36,24 @@ const Home = () => {
     }, [])
 
     return (
-        <Layout title='Home Page' description='E-commerce App'>
-            {JSON.stringify(productsByArrival)}
-            <hr/>
-            {JSON.stringify(productsBySell)}
+        <Layout title='Home Page' description='E-commerce App' className='container-fluid'>
+             <h2 className='mb-4'>
+               New Arrivals
+            </h2>
+            <div className='row'>
+                {productsByArrival.map((product, i) => {
+                    return <Card key={i} product={product} />
+                })}
+            </div>
+            
+            <h2 className='mb-4'>
+                Best Sellers
+            </h2>
+            <div className='row'>
+                {productsBySell.map((product, i) => {
+                    return <Card key={i} product={product} />
+                })}
+            </div>
         </Layout>
     )
 }
