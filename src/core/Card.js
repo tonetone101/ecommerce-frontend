d60da8ef63f35e982893ab3bb2fbc2ede2e17f08
Antkeo1 <sleav143@gmail.com>
2020-04-04 16:19:12 -0400

    added some styling to jumbotron

--- src/core/Card.js
+++ src/core/Card.js
@@ -18,7 +18,7 @@ const Card = ({product}) => {
                         ${product.price}
                     </p>
                     <Link to='/'>
-                        <button className='btn btn-outline-primary mt-2 mb-2'>
+                        <button className='btn btn-outline-primary mt-2 mb-2 mr-2'>
                             View Product
                         </button>
                     </Link>
